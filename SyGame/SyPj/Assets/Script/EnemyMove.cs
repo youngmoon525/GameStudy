f0d6ad65398f20fb95cd2ece0e1ef9e884ac8fc2
YmKim <youngmoon525@naver.com>
2023-09-17 21:42:33 +0900

    abc
    c

--- SyGame/SyPj/Assets/Script/EnemyMove.cs
+++ SyGame/SyPj/Assets/Script/EnemyMove.cs
@@ -7,10 +7,13 @@ public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     public int nextMove;
-
+    Animator anim;
+    SpriteRenderer spriteRenderer;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();    
         Invoke("think", 5);
         //think();
     }
@@ -27,14 +30,46 @@ public class EnemyMove : MonoBehaviour
      void FixedUpdate()
     {
      
-        rigid.velocity = new Vector2(nextMove, rigid.velocity.y); 
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
+
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        if (rigid.velocity.y < 0)
+        {
+
+
+            RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1f, LayerMask.GetMask("Platform"));
+            if (rayHit.collider == null)
+            {
+                turn();
+            }
+        }
+
     }
 
     void think()
     {
         nextMove = Random.Range(-1, 2);
+        float nextThinkTime = Random.Range(2f, 5f);
+    
+        anim.SetInteger("walkSpeed", nextMove);
+        if(nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == 1;
+        }
+        Invoke("think", nextThinkTime);
 
+
+    }
+
+    void turn()
+    {
+        nextMove = nextMove * -1;
+        CancelInvoke();
         Invoke("think", 5);
+        spriteRenderer.flipX = nextMove == 1;
     }
 
 }
