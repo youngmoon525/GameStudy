1c1b666236b4a809bf9fe238e829494874e1145c
ym <youngmoon525@naver.com>
2023-10-10 13:07:35 +0900

    1
    123

--- SyGame/SyPj/Assets/TopDownScript/TopDownPlayer.cs
+++ SyGame/SyPj/Assets/TopDownScript/TopDownPlayer.cs
@@ -8,18 +8,35 @@ public class TopDownPlayer : MonoBehaviour
     float h;
     float v;
     Rigidbody2D rigid;
+    bool isHorizonMove;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
     private void Update()
-    {
-        h = Input.GetAxisRaw("Horizontal") * speed;
-        v = Input.GetAxisRaw("Vertical") * speed;
+    { 
+        h = Input.GetAxisRaw("Horizontal")  ;
+        v = Input.GetAxisRaw("Vertical")  ;
+
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+
+        if (hDown || vUp)
+            isHorizonMove = true;
+        else if (vDown || hUp)
+            isHorizonMove = false;
+
+
+
     }
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * speed;
     }
 }
